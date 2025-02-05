0a12399803a0a79f6897b263ba3ffb0f888b3895
aonajin <onag24di@student.ju.se>
2025-02-05 10:50:27 +0100

    Scroll to top behavior, added w javascript

---
+++ js/main.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const backToTopButton = document.getElementById("backToTop");
+  
+    window.addEventListener("scroll", function () {
+      if (window.scrollY > 200) {
+        backToTopButton.classList.add("show");
+        backToTopButton.classList.remove("hide");
+      } else {
+        backToTopButton.classList.remove("show");
+        backToTopButton.classList.add("hide");
+      }
+    });
+  
+    backToTopButton.addEventListener("click", function () {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+});
+//back to top function
