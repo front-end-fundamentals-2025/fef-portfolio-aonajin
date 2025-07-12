c833a7a4a371581b3baff2d4d7102a581a468e03
aonajin <onag24di@student.ju.se>
2025-07-12 22:59:55 +0200

    working vers w keyframes (made sure)

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const backToTopButton = document.getElementById("backToTop");
-  
-    window.addEventListener("scroll", function () {
-      if (window.scrollY > 200) {
-        backToTopButton.classList.add("show");
-        backToTopButton.classList.remove("hide");
-      } else {
-        backToTopButton.classList.remove("show");
-        backToTopButton.classList.add("hide");
-      }
-    });
-  
-    backToTopButton.addEventListener("click", function () {
-      window.scrollTo({ top: 0, behavior: "smooth" });
-    });
+  const backToTopButton = document.getElementById("backToTop");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 100) {
+      backToTopButton.classList.add("show");
+      backToTopButton.classList.remove("hide");
+    } else {
+      backToTopButton.classList.remove("show");
+      backToTopButton.classList.add("hide");
+    }
+  });
+
+  backToTopButton.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
 });
-//back to top
+
+//back to top logic
